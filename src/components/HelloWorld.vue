35cdc027b2143e42ed7c50b57035ca90421ecbf0
Krzysztof Platis <platonn.git@gmail.com>
2018-05-12 03:00:59 +0200

    MVP done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,178 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
+  <div class="calendar">
+    <ul class="week">
+      <li v-for="weekDayName in weekDayNames" v-bind:key="weekDayName" class="cell cell--dayName">
+        {{ weekDayName }}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    <ul v-for="week in calendar" v-bind:key="week.week" class="week">
+      <li v-for="day in week.days" v-bind:key="day.date()" class="cell cell--day"
+          v-bind:class="{ 'cell--borderTop': hasBorderTop(day, week), 'cell--borderLeft': hasBorderLeft(day, week) }">
+        {{ formatDay(day) }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import * as moment from 'moment'
+import 'moment/locale/pl'
+
+//config:
+window.moment = moment //spike
+moment.updateLocale('pl', {
+  weekdaysShort : ['ND', 'PON', 'WT', 'ŚR', 'CZW', 'PT', 'SB']
+})
+moment.locale('pl')
+
+
+//spike todo: calculate from given day, currently today
+const WEEKS_NUMBER = 12
+const FIRST_DAY_OF_WEEK = 1; // monday
+const weekDayNamesStandard = moment.weekdaysShort()
+const shiftedPartOfWeek = weekDayNamesStandard.slice(0, FIRST_DAY_OF_WEEK)
+const restPartOfWeek = weekDayNamesStandard.slice(FIRST_DAY_OF_WEEK, 7)
+const WEEK_DAY_NAMES = restPartOfWeek.concat(shiftedPartOfWeek);
+
+const DATE_FORMATS = [
+  'DD',
+  'D.M',
+  'DD.MM',
+  'DD.MM.YYYY',
+  'D-M',
+  'DD-MM',
+  'DD-MM-YYYY'
+]
+
+const constructCalendar = (startDayString) => {
+    const startDay = startDayString ?
+      moment(startDayString, DATE_FORMATS) :
+      moment().startOf('day');
+      console.log(startDayString, startDay.format('YYYY-MM-DD')) //spike
+    const year = startDay.year();
+
+    let calendar = [];
+    const startWeek = startDay.week();
+    const endWeek = startWeek + WEEKS_NUMBER;
+
+    for(var week = startWeek; week<endWeek; week++){
+      calendar.push({
+        week: week,
+        days: Array(7).fill(0).map((n, i) => moment().year(year).week(week).startOf('week').clone().add(n + i, 'day'))
+      })
+    }
+    return calendar;
+}
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      calendar: [],
+      weekDayNames: WEEK_DAY_NAMES
+    }
+  },
+  created() {
+    this.calendar = constructCalendar(this.$route.params.date)
+  },
+
+  watch: {
+    '$route' () {
+      this.calendar = constructCalendar(this.$route.params.date)
+    }
+  },
+
+  methods: {
+    formatDay(day) {
+      if(this.shouldShowMonthName(day)) {
+        return day.format("D MMMM")
+      } else {
+        return day.format("D")
+      }
+    },
+    shouldShowMonthName(day) {
+      return this.isFirstDayInCalendar(day) || this.isFirstDayOfMonth(day)
+    },
+    isFirstDayOfMonth(day){
+      return day.date() === 1
+    },
+    isFirstDayInCalendar(day) {
+      // debugger
+      return day === this.calendar[0].days[0]
+    },
+
+    hasBorderLeft(day, week){
+      const isFirstDayOfWeek = day === week[0]
+      
+      return !isFirstDayOfWeek && this.isFirstDayOfMonth(day)
+    },
+
+    hasBorderTop(day) {
+      const monthOfDay = day.month()
+      const monthOfDayBeforeOneWeek = day.clone().subtract(1, 'week').month()
+      const isFirstWeekOfCalendar = this.calendar[0].days.includes(day)
+
+      return !isFirstWeekOfCalendar && monthOfDay !== monthOfDayBeforeOneWeek;
+    },
+    
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.calendar {
+  --border-color: black;
+  --cell-width: 25mm;
+  --cell-height: 21mm;
+  
+  text-align: left;
+  border-width: 0 1px 1px 0;
+  border-style: solid;
+  border-color: var(--border-color);
+  display: inline-block;
 }
+
+.week {
+  border-top: 1px solid var(--border-color);
+}
+
+.cell {
+  width: var(--cell-width);
+  display: inline-block;
+  font-size: 0.8em;
+  font-weight: 900;
+  border-left: 1px solid var(--border-color);
+  margin: 0;
+  background-color: white;
+}
+
+.cell--dayName {
+  text-align: center;
+    height: 3mm;
+    padding: 1mm;
+    border-bottom: 1px solid black;
+    margin-bottom: 1px;
+}
+
+.cell--day {
+  padding: 1mm;
+  height: var(--cell-height);
+}
+
+.cell--borderTop {
+  border-top: 1px solid var(--border-color);
+  height: calc(var(--cell-height) - 1px);
+}
+
+.cell--borderLeft {
+  border-left: 2px solid var(--border-color);
+  width: calc(var(--cell-width) - 1px);
+}
+
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  margin: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/:date', component: HelloWorld },
+    { path: '/', component: HelloWorld }
+  ]
+})
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
